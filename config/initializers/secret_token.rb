20d3f0bfa643807345c087e8eb075c702eda3737
jhadvig <j.hadvig@gmail.com>
2012-12-04 14:44:52 +0100

    rails 3.0.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KatelloScholar::Application.config.secret_token = 'bc58870c70d1a05b58086b45c5866256cd8bf14c67e47aa08d3b82739887756476d4e45b966fc668c33603a1f273cdf091999f0c1978279357a85809733c8d1e'
